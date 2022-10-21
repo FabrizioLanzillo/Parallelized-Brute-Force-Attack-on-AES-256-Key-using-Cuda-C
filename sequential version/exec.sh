621bfb3f65e05077e90501ccbea77779cce53b2a
Fabrizio Lanzillo <86826030+FabrizioLanzillo@users.noreply.github.com>
2022-10-21 17:37:33 +0200

    Update exec.sh

--- sequential version/exec.sh
+++ sequential version/exec.sh
@@ -7,7 +7,7 @@ do
    ./run.exe 20 # 2 bytes and 4 bits
    ./run.exe 24 # 3 bytes
    ./run.exe 27 # 3 bytes and 3 bits
-   ./run.exe 30 # 3 bytes and 3 bits
+   ./run.exe 30 # 3 bytes and 6 bits
    #./run.exe 32 # 4 bytes
 
 done
